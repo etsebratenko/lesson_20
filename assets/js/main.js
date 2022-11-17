074cdfc6b3ea8ed8e79c9a3dce0cbd73fa4742ff
Kate <etsebratenko@gmail.com>
2022-11-17 22:21:43 +0200

    базовая настройка

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// управление меню
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+const body = document.querySelector('body');
+
+menuBtn.addEventListener('click', (e) => {
+  menuBtn.classList.toggle('active');
+  body.classList.toggle('active');
+  if (menuList.classList.contains('active')) {
+    setTimeout(() => menuList.classList.toggle('active'), 500);
+  } else {
+    menuList.classList.toggle('active');
+  }
+});
