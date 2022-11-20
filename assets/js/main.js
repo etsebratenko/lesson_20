4ebf9f9aaa0f444b2b9d5546b5d7f02017e2303b
Kate <etsebratenko@gmail.com>
2022-11-20 18:55:02 +0200

    оформление header

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,33 @@ menuBtn.addEventListener('click', (e) => {
     menuList.classList.toggle('active');
   }
 });
+
+
+// считали DOM элементы
+const headersList = document.querySelectorAll('.slider__header');
+const notesList = document.querySelectorAll('.slider__note');
+const indicatorsList = document.querySelectorAll('.slider__indicator');
+const descriptionsList = document.querySelectorAll('.slider__note-description');
+
+let index = 0;
+let interval = 5000;
+let heightsArr = [];
+let heightMax = null;
+
+setInterval(() => {
+  // снимаем классы active для первых элементов
+  headersList[index].classList.toggle('active');
+  notesList[index].classList.toggle('active');
+  indicatorsList[index].classList.toggle('active');
+  // увеличиваем индекс, пока не превышено количество элементов
+  index = (index + 1) % headersList.length;
+  // ставим классы active следующим элементам
+  headersList[index].classList.toggle('active');
+  notesList[index].classList.toggle('active');
+  indicatorsList[index].classList.toggle('active');
+}, interval);
+
+// вычисление и изменение высоты блока описания под максимальный текст
+descriptionsList.forEach(el => heightsArr.push(el.clientHeight));
+heightMax = Math.max(...heightsArr);
+descriptionsList.forEach(el => el.style.height = `${heightMax}px`);
